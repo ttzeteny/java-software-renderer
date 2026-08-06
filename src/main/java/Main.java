b438074c1f5340eebd1d997e71f6180da9d74a25
Zeteny <ttzeteny@gmail.com>
2026-08-06 20:05:43 +0200

    Add flat shading

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -91,6 +91,29 @@ public class Main {
                     double triangleArea =
                             (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
 
+                    Vertex ab = new Vertex(
+                            v2.x - v1.x,
+                            v2.y - v1.y,
+                            v2.z - v1.z
+                    );
+                    Vertex ac = new Vertex(
+                            v3.x - v1.x,
+                            v3.y - v1.y,
+                            v3.z - v1.z
+                    );
+                    Vertex norm = new Vertex(
+                            ab.y * ac.z - ab.z * ac.y,
+                            ab.z * ac.x - ab.x * ac.z,
+                            ab.x * ac.y - ab.y * ac.x
+                    );
+                    double normalLength =
+                            Math.sqrt(norm.x * norm.x + norm.y * norm.y + norm.z * norm.z);
+                    norm.x /= normalLength;
+                    norm.y /= normalLength;
+                    norm.z /= normalLength;
+
+                    double angleCos = Math.abs(norm.z);
+
                     for (int y = minY; y <= maxY; y++) {
                         for (int x = minX; x <= maxX; x++) {
                             double b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
@@ -100,13 +123,13 @@ public class Main {
                                 double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
                                 int zIndex = y * img.getWidth() + x;
                                 if (zBuffer[zIndex] < depth) {
-                                    img.setRGB(x, y, t.color.getRGB());
+                                    Color shaded = getShade(t.color, angleCos);
+                                    img.setRGB(x, y, shaded.getRGB());
                                     zBuffer[zIndex] = depth;
                                 }
                             }
                         }
                     }
-
                 }
 
                 g2.drawImage(img, 0, 0, null);
@@ -121,5 +144,17 @@ public class Main {
 
         frame.setSize(500, 500);
         frame.setVisible(true);
+    }
+
+    public static Color getShade(Color color, double shade) {
+        double redLinear = Math.pow(color.getRed(), 2.4) * shade;
+        double greenLinear = Math.pow(color.getGreen(), 2.4) * shade;
+        double blueLinear = Math.pow(color.getBlue(), 2.4) * shade;
+
+        int red = (int) Math.pow(redLinear, 1/2.4);
+        int green = (int) Math.pow(greenLinear, 1/2.4);
+        int blue = (int) Math.pow(blueLinear, 1/2.4);
+
+        return new Color(red, green, blue);
     }
 }
