8b88bd8c4662ee4f0bd90632c7a931b400f29d88
Zeteny <ttzeteny@gmail.com>
2026-08-06 19:28:20 +0200

    Add horizontal rotation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -44,18 +44,31 @@ public class Main {
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
 
+                double heading = Math.toRadians(headingSlider.getValue());
+                Matrix3 transform = new Matrix3(new double[] {
+                        Math.cos(heading), 0, -Math.sin(heading),
+                        0, 1, 0,
+                        Math.sin(heading), 0, Math.cos(heading)
+                });
+
                 g2.translate(getWidth() / 2, getHeight() / 2);
                 g2.setColor(Color.WHITE);
                 for (Triangle t : triangles) {
+                    Vertex v1 = transform.transform(t.v1);
+                    Vertex v2 = transform.transform(t.v2);
+                    Vertex v3 = transform.transform(t.v3);
                     Path2D path = new Path2D.Double();
-                    path.moveTo(t.v1.x, t.v1.y);
-                    path.lineTo(t.v2.x, t.v2.y);
-                    path.lineTo(t.v3.x, t.v3.y);
+                    path.moveTo(v1.x, v1.y);
+                    path.lineTo(v2.x, v2.y);
+                    path.lineTo(v3.x, v3.y);
                     path.closePath();
                     g2.draw(path);
                 }
             }
         };
+
+        headingSlider.addChangeListener(e -> renderPanel.repaint());
+        pitchSlider.addChangeListener(e -> renderPanel.repaint());
 
         pane.add(renderPanel, BorderLayout.CENTER);
 
